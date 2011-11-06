9c4af652c1c75bd19e35ae2e7192aaa6418b6297
Kris Khaira <kris.khaira@gmail.com>
2011-11-06 12:33:41 +0800

    Updated fix_perms.sh.

--- fix_perms.sh
+++ fix_perms.sh
@@ -1,11 +1,20 @@
-chmod -R 777 app/tmp/
-chmod -R 777 app/media/
-chmod -R 777 app/webroot/css/
-chmod -R 777 app/webroot/js
-chmod -R 777 app/webroot/img/
-chmod -R 777 app/webroot/files/
-chmod -R 777 core/vendors/securimage/
-chmod -R 777 app/vendors/shells/cron.sh 
-chmod -R 777 app/vendors/shells/cron.php
-chmod -R 777 core/cake/console/cake
+mkdir app/media
+mkdir app/tmp
+mkdir app/webroot/js
+mkdir app/webroot/img
+mkdir app/webroot/css
+mkdir app/webroot/files
+mkdir core/cake/console/cake
+mkdir core/vendors/securimage
 
+find app/media -type d -exec chmod 777 {} \;
+find app/tmp -type d -exec chmod 777 {} \;
+find app/vendors/shells/cron.sh -type d -exec chmod 777 {} \;
+find app/vendors/shells/cron.php -type d -exec chmod 777 {} \;
+find core/cake/console/cake -type d -exec chmod 777 {} \;
+find core/vendors/securimage -type d -exec chmod 777 {} \;
+touch core/vendors/shells/cron.sh
+chmod 777 core/vendors/shells/cron.sh
+touch core/vendors/shells/cron.php
+chmod 777 core/vendors/shells/cron.php
+chmod 777 core/cake/console/cake
