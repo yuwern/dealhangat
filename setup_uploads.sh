93b5aefab872df51308edb8ffc9b00f819ab4c90
Kris Khaira <kris.khaira@gmail.com>
2011-11-06 15:26:45 +0800

    Added app/media to setup_uploads.sh.

--- setup_uploads.sh
+++ setup_uploads.sh
@@ -7,3 +7,4 @@ rsync -avzpP $SOURCE/app/webroot/img/normal_thumb app/webroot/img/
 rsync -avzpP $SOURCE/app/webroot/img/micro_thumb app/webroot/img/
 rsync -avzpP $SOURCE/app/webroot/img/small_big_thumb app/webroot/img/
 rsync -avzpP $SOURCE/app/webroot/img/small_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/media app/media
