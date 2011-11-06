fd3a61c53f8d1df7c29b736df5657d247c69fe99
Kris Khaira <kris.khaira@gmail.com>
2011-11-06 13:49:08 +0800

    Added setup_uploads.sh for downloading uploads from staging and also renamed fix_perms.sh to setup_perms.sh.

---
+++ setup_uploads.sh
@@ -0,0 +1,9 @@
+SOURCE="user@dealhangat.com:~/public_staging"
+
+rsync -avzpP $SOURCE/app/webroot/img/big_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/medium_big_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/medium_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/normal_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/micro_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/small_big_thumb app/webroot/img/
+rsync -avzpP $SOURCE/app/webroot/img/small_thumb app/webroot/img/
